caa23556a67d7a5766f43e83fcdcf9cedff269a0
Victor <sussahin@ya.ru>
2015-11-04 13:18:51 +0300

    Add viterby

--- Python/Viterbi/inp.py
+++ Python/Viterbi/inp.py
@@ -1,158 +1,61 @@
-penalty = -1
-alignment_price = 1
-skip_penalty = -1
+begin = {'+': 0.5, '-': 0.5}
 
-base_prices = {
-	'A': {'A': alignment_price, 'T': penalty, 'G': penalty, 'C': penalty},
-	'T': {'A': penalty, 'T': alignment_price, 'G': penalty, 'C': penalty},
-	'G': {'A': penalty, 'T': penalty, 'G': alignment_price, 'C': penalty},
-	'C': {'A': penalty, 'T': penalty, 'G': penalty, 'C': alignment_price},
+transitions = {
+	'+': {'+': 0.5, '-': 0.5},
+	'-': {'+': 0.5, '-': 0.5},
 }
 
-complex_prices = {
-	'A': {'A': 1, 'T': -1, 'G': -1, 'C': -1},
-	'T': {'A': -2, 'T': 1, 'G': -1, 'C': -1},
-	'G': {'A': -1, 'T': -1, 'G': 1, 'C': -1},
-	'C': {'A': -1, 'T': -1, 'G': -1, 'C': 1},
+generation = {
+	'+': {'0': 1, '1': 0},
+	'-': {'0': 0, '1': 1}
+	# '+': {'0': 0.99, '1': 0.01},
+	# '-': {'0': 0.01, '1': 0.99}
 }
 
 
 class Cell:
-	__skip_penalty__ = {
-		False: skip_penalty,
-		True: -1,
-	}
-
-	def __init__(self, value, dir='', level=0):
-		self.value = value
-		self.dir = dir
-		self.level = level
-
-	def with_vert_skip(self):
-		return Cell(self.value + Cell.__skip_penalty__[self.level == 1], 'U', 1)
-
-	def with_hor_skip(self):
-		return Cell(self.value + Cell.__skip_penalty__[self.level == -1], 'L', -1)
-
-	def without_skip(self, price):
-		return Cell(self.value + price, 'D', 0)
+	def __init__(self, probability, prev_state):
+		assert isinstance(prev_state, str)
+		assert isinstance(probability, (float, int))
+		self.probability = probability
+		self.prev_state = prev_state
 
 
-# Как выводить выравнивание?
-def get_local_alignment(seq1, seq2, prices):
-	assert isinstance(seq1, str)
-	assert isinstance(seq2, str)
+def get_states_sequence(seq):
+	assert isinstance(seq, str)
 
-	matrix = build_matrix(prices, seq1, seq2, True)
+	matrix = build_matrix(seq)
 
+	print(sorted(matrix[0].keys()))
 	for row in matrix:
-		print([c.value for c in row])
+		print([row[c].probability for c in sorted(row.keys())])
 
-	max_row, max_col = 0, 0
-	for row in range(len(seq2)):
-		for col in range(len(seq1)):
-			if matrix[row][col].value > matrix[max_row][max_col].value:
-				max_row, max_col = row, col
+	current_key = max(transitions.keys(), key=lambda key: matrix[-1][key].probability)
+	res = '' + current_key
+	for row in matrix[-2::-1]:
+		res = current_key + res
+		current_key = row[current_key].prev_state
 
-	# if (max_row == len(seq2) - 1 and max_col == len(seq1) - 1):
-	# 	max_col += 1
-	# 	max_row += 1
-
-	res1 = ''
-	res2 = ''
-	row, col = max_row, max_col
-	while row > 0 and col > 0 and matrix[row][col].value >= 0:
-		if matrix[row][col].dir == 'U':
-			res1 = '-' + res1
-			res2 = seq2[row - 1] + res2
-			row -= 1
-		elif matrix[row][col].dir == 'L':
-			res1 = seq1[col - 1] + res1
-			res2 = '-' + res2
-			col -= 1
-		else:
-			res1 = seq1[col - 1] + res1
-			res2 = seq2[row - 1] + res2
-			col -= 1
-			row -= 1
-
-	return res1, res2
+	return res
 
 
-def get_global_alignment(seq1, seq2, prices):
-	assert isinstance(seq1, str)
-	assert isinstance(seq2, str)
+def build_matrix(seq):
+	assert isinstance(seq, str)
 
-	matrix = build_matrix(prices, seq1, seq2, False)
+	matrix = [{state: Cell(0, '-') for state in transitions.keys()} for _ in seq]
+	for state in begin.keys():
+		matrix[0][state] = Cell(generation[state][seq[0]], 'b')
 
-	for row in matrix:
-		print([c.value for c in row])
-	res1 = ''
-	res2 = ''
-	row, col = len(seq2), len(seq1)
-	while row > 0 and col > 0:
-		if matrix[row][col].dir == 'U':
-			res1 = '-' + res1
-			res2 = seq2[row - 1] + res2
-			row -= 1
-		elif matrix[row][col].dir == 'L':
-			res1 = seq1[col - 1] + res1
-			res2 = '-' + res2
-			col -= 1
-		else:
-			res1 = seq1[col - 1] + res1
-			res2 = seq2[row - 1] + res2
-			col -= 1
-			row -= 1
-
-	while col > 0:
-		res1 = seq1[col - 1] + res1
-		res2 = '-' + res2
-		col -= 1
-
-	while row > 0:
-		res1 = '-' + res1
-		res2 = seq2[row - 1] + res2
-		row -= 1
-
-	return res1, res2
-
-
-def build_matrix(prices, seq1, seq2, is_local):
-	assert isinstance(seq1, str)
-	assert isinstance(seq2, str)
-	assert isinstance(is_local, bool)
-
-	matrix = [[Cell(0) for _ in range(len(seq1) + 1)] for _ in range(len(seq2) + 1)]
-	matrix[0] = [Cell(skip_penalty * x) for x in range(len(seq1) + 1)]
-	for y in range(len(seq2) + 1):
-		matrix[y][0].value = y * skip_penalty
-	for row in range(1, len(seq2) + 1):
-		for col in range(1, len(seq1) + 1):
-			cell = max(matrix[row - 1][col].with_vert_skip(),
-						matrix[row][col - 1].with_hor_skip(),
-						matrix[row - 1][col - 1].without_skip(prices[seq1[col - 1]][seq2[row - 1]]),
-						key=lambda x: x.value)
-
-			if is_local and cell.value < 0:
-				cell.value = 0
-
-			matrix[row][col] = cell
+	for i in range(1, len(seq)):
+		for cur_state in transitions.keys():
+			test = [matrix[i - 1][prev_state].probability * transitions[prev_state][cur_state] for prev_state in transitions.keys()]
+			best_state = max(transitions.keys(), key=lambda prev_state: matrix[i - 1][prev_state].probability * transitions[prev_state][cur_state])
+			matrix[i][cur_state] = Cell(matrix[i - 1][best_state].probability * transitions[best_state][cur_state] * generation[best_state][seq[i]], best_state)
 
 	return matrix
 
-
+#TODO logarithms
 if __name__ == '__main__':
-	str1 = input()
-	str2 = input()
-	# res1, res2 = get_global_alignment(str1, str2, base_prices)
-	res1, res2 = get_local_alignment(str1, str2, complex_prices)
-	#res1, res2 = get_global_alignment('ACTGATTGCT', 'ATATG', complex_prices)
-	print(res1)
-	print(res2)
-
-# AAATTTGCCGC
-# AAACCCTGCGG
-
-# AAATTGC
-# AA-T--C
+	# str = input()
+	string = '000111'
+	print(get_states_sequence(string))
